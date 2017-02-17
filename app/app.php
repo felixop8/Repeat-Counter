882d6d05d9c349ebda4118880de6064f2f8b3d74
felixop8 <felixop8@gmail.com>
2017-02-17 13:21:44 -0800

    create html pages in views

---
+++ app/app.php
@@ -0,0 +1,27 @@
+<?php
+    // stops php timestamp error from occuring
+    date_default_timezone_set('America/Los_Angeles');
+    // connects created classes to the project
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/RepeatCounter.php";
+
+    // creates a new app using the Silex micro framework
+    $app = new Silex\Application();
+    $app['debug'] = true;
+
+    // adds Twig as the service provider for the app templates and sets the twig path to where the templates will be located
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../web/views'));
+
+
+    // route main page
+   $app->get("/", function() use ($app) {
+       return $app['twig']->render('forms.html.twig');
+   });
+
+  //  routing for after form is submitted
+   $app->post("/result", function() use ($app) {
+       return $app['twig']->render('result.html.twig');
+   });
+
+   return $app;
+ ?>
