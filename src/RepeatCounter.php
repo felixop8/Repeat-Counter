b9911f3a4f8251a08f9122e6458bcd689ecca275
felixop8 <felixop8@gmail.com>
2017-02-17 11:59:00 -0800

    pass test for differents punctuations

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -6,7 +6,22 @@
 
             $lower_word = strtolower($single_word);
             $lower_phrase = strtolower($phrase);
-            $array_of_words = explode(" ", $lower_phrase);
+            $phrase_splited = str_split($lower_phrase);
+            $punctuation_array= array('.', ',', ';', ':', '-', ' " ', ' \' ', '(', ')', '-', '?', '!' );
+
+
+            foreach ($phrase_splited as $key => $value) {
+                foreach ($punctuation_array as $punctuation) {
+                    if ($value== $punctuation)
+                    {
+                        unset($phrase_splited[$key]);
+                    }
+                }
+            }
+            $phrase_splited = array_values($phrase_splited);
+
+            $phrase_joined = join("", $phrase_splited);
+            $array_of_words = explode(" ", $phrase_joined);
             $counter = 0;
             foreach ($array_of_words as $word) {
                 if ($lower_word == $word)
--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -27,5 +27,17 @@
             // Assert
             $this->assertEquals(2, $result);
         }
+
+        function test_words_with_punctuation()
+        {
+            // Arrange
+            $test_repetition = new RepeatCounter;
+            $input_word = "Fear";
+            $input_string_of_words = "The only thing we should fear, is :fear itself";
+            // Act
+            $result = $test_repetition->CounterRepetition($input_word, $input_string_of_words);
+            // Assert
+            $this->assertEquals(2, $result);
+        }
     }
  ?>
