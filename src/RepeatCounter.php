83a2ea5132bb746c6954319a4fd833349b6c2554
felixop8 <felixop8@gmail.com>
2017-02-22 12:55:31 -0800

    refactor method countRepeat in the class RepeatCounter

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -8,17 +8,20 @@
             // Remove whitespaces
             $user_word = trim($user_word, " ");
 
+
             // Make sentence lowercase
             $user_phrase = strtolower($phrase);
+            // Take user phrase and split in an array of words
             $user_phrase = explode(" ", $user_phrase);
-            // var_dump($user_phrase);
-            // $phrase_splited = str_split($user_phrase);
-            $punctuation_array= array(".", "," , ";" , ":" , "-",  '"' , "'", "(" , ")" , "-" , "?" , "!");
-            $count = 0;
 
+
+            $special_chars = array(".", "," , ";" , ":" , "-",  '"' , "'", "(" , ")" , "-" , "?" , "!");
+
+
+            $count = 0;
             foreach ($user_phrase as $word) {
-                foreach ($punctuation_array as $punctuation) {
-                    $word = trim($word, $punctuation);
+                foreach ($special_chars as $character) {
+                    $word = trim($word, $character);
                 }
                 $word = strtolower($word);
                 // var_dump($word_in_text);
